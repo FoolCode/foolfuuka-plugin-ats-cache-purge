97e76cc266e84f897fe967981565e704fc22ee5c
Hung Tran <oohnoitz@gmail.com>
2015-01-25 22:08:03 -0600

    updated plugin hooks

--- bootstrap.php
+++ bootstrap.php
@@ -24,7 +24,7 @@ class HHVM_ATSCachePurge
                     ->register('foolfuuka-plugin.ats_cache_purge', 'Foolz\Foolfuuka\Plugins\ATSCachePurge\Model\ATSCachePurge')
                     ->addArgument($context);
 
-                Event::forge('Foolz\Foolframe\Model\Context::handleWeb#obj.afterAuth')
+                Event::forge('Foolz\FoolFrame\Model\Context::handleWeb#obj.afterAuth')
                     ->setCall(function ($result) use ($context) {
                         // don't add the admin panels if the user is not an admin
                         if ($context->getService('auth')->hasAccess('maccess.admin')) {
@@ -41,7 +41,7 @@ class HHVM_ATSCachePurge
                                 )
                             );
 
-                            Event::forge('Foolz\Foolframe\Controller\Admin::before#var.sidebar')
+                            Event::forge('Foolz\FoolFrame\Controller\Admin::before#var.sidebar')
                                 ->setCall(function ($result) {
                                     $sidebar = $result->getParam('sidebar');
                                     $sidebar[]['plugins'] = [
@@ -52,7 +52,7 @@ class HHVM_ATSCachePurge
                         }
                     });
 
-                Event::forge('Foolz\Foolfuuka\Model\Media::delete#call.beforeMethod')
+                Event::forge('Foolz\FoolFuuka\Model\Media::delete#call.beforeMethod')
                     ->setCall(function ($result) use ($context) {
                         $context->getService('foolfuuka-plugin.ats_cache_purge')->beforeDeleteMedia($result);
                     });
